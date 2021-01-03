ebc08703be1a4664356e63dd57eb72fd9b1ca918
joaquinluis <joaquin_akerman@hotmail.com>
2021-01-03 01:44:32 -0300

    Agrega pokedex basico
    Se pueden ver los primeros pokemones

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function listarPokemones() {
 		.then((lista20Pokemones) => lista20Pokemones.json())
 
 		.catch((error) =>
-			console.console.error(
+			console.error(
 				"falló cargar la lista de pokemones, intente nuevamente",
 				error
 			)
@@ -29,13 +29,67 @@ function inicializar() {
 
 inicializar();
 
-function armarDatalist(infoPokemon) {
-	console.log(infoPokemon);
-	infoPokemon.forEach(($pokemon) => {
+document
+	.querySelector("#boton-ver-detalles")
+	.addEventListener("click", function () {
+		const pokemonSeleccionado = document.querySelector("#selector-pokemon")
+			.value;
+		const urlPokemonSeleccionado = document
+			.querySelector(`#lista-pokemones [value="${pokemonSeleccionado}"]`)
+			.getAttribute("data-url");
+		obtenerDatosParaTarjeta(urlPokemonSeleccionado).then(
+			(infoJSONDelPokemon) => {
+				mostrarPropiedadesPokemon(infoJSONDelPokemon);
+			}
+		);
+	});
+
+function armarDatalist(infoPokemones) {
+	infoPokemones.forEach(($pokemon) => {
 		let $listaDePokemones = document.querySelector("#lista-pokemones");
 		let option = document.createElement("option");
 		option.value = $pokemon.name;
 		option.dataset.url = $pokemon.url;
 		$listaDePokemones.append(option);
 	});
+}
+
+function obtenerDatosParaTarjeta(urlDelPokemon) {
+	return fetch(urlDelPokemon)
+		.then((specsDelPokemon) => specsDelPokemon.json())
+
+		.catch((error) =>
+			console.error("falló cargar el pokemon, intente nuevamente", error)
+		);
+}
+
+function mostrarPropiedadesPokemon(datosJSONDelPokemon) {
+	limpiarCampos();
+	let $tarjetaInfoPokemon = document.querySelector("#info-tarjeta");
+	console.log(datosJSONDelPokemon);
+	let $imagenDelPokemon = document.querySelector("#imagen-pokemon");
+	let urlImagenPokemon = datosJSONDelPokemon.sprites.front_default;
+	$imagenDelPokemon.src = urlImagenPokemon;
+	let nombre = document.createElement("p");
+	nombre.class = "card-text";
+	nombre.innerText = `Nombre: ${datosJSONDelPokemon.name}`;
+	$tarjetaInfoPokemon.append(nombre);
+	let tipo = document.createElement("p");
+	tipo.class = "card-text";
+	tipo.innerText = `Tipo: ${datosJSONDelPokemon.types[0].type.name}`;
+	$tarjetaInfoPokemon.append(tipo);
+	let peso = document.createElement("p");
+	peso.class = "card-text";
+	peso.innerText = `Peso: ${datosJSONDelPokemon.weight}`;
+	$tarjetaInfoPokemon.append(peso);
+	let altura = document.createElement("p");
+	altura.class = "card-text";
+	altura.innerText = `Altura: ${datosJSONDelPokemon.height}`;
+	$tarjetaInfoPokemon.append(altura);
+}
+
+function limpiarCampos() {
+	document.querySelector("#info-tarjeta").innerHTML = "";
+	document.querySelector("#selector-pokemon").value = "";
+	//inputDatalist
 }
